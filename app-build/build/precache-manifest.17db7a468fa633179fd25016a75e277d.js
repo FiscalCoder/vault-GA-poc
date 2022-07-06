c95d9142267cf0b9423e5e9e2b8d3204c80336d3
bharghav-raman <bharghav.raman@tarento.com>
2022-07-06 19:35:23 +0530

    edit: change deployment to another instance

---
+++ app-build/build/precache-manifest.17db7a468fa633179fd25016a75e277d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915d9e7ce426cf85ea8ba89fc82f289a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c794669759c17dd35e07",
+    "url": "/static/css/2.c8b95c23.chunk.css"
+  },
+  {
+    "revision": "9a00912ee309025a02a6",
+    "url": "/static/css/main.3089337e.chunk.css"
+  },
+  {
+    "revision": "c794669759c17dd35e07",
+    "url": "/static/js/2.4d890bb1.chunk.js"
+  },
+  {
+    "revision": "9a00912ee309025a02a6",
+    "url": "/static/js/main.ea1dd969.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
